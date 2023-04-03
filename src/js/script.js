3e7419dc4cb9ccde67397cf3d2fe27fa430b29fe
jahon <jahongir.danaboyev@mail.ru>
2023-04-03 21:31:57 +0300

    carousel

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,24 @@
-alert(1);
+$(document).ready(function(){
+	$('.carousel__inner').slick({
+		speed: 1300,
+		adaptiveHeight: true,
+		autoplay: true,
+		autoplaySpeed: 3000,
+		prevArrow:'<button type="button" class="slick-prev "><img src ="icons/left.svg"> </button>',
+		nextArrow:'<button type="button" class="slick-next"><img src ="icons/right.svg"> </button>',
+		responsive: [
+			{
+			  breakpoint: 992,
+			  settings: {
+				arrows: false
+			  }
+			},
+			{
+			  breakpoint: 767,
+			  settings: {
+				arrows: false,
+			  }
+			}
+		  ]
+	  });
+}); 
