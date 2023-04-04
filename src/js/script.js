c56bf8302e4f2c726acc8427fff8e6e8c75a2fff
jahon <jahongir.danaboyev@mail.ru>
2023-04-04 14:38:45 +0300

    carosel_catalog

--- src/js/script.js
+++ src/js/script.js
@@ -2,21 +2,31 @@ $(document).ready(function(){
 	$('.carousel__inner').slick({
 		speed: 1300,
 		adaptiveHeight: true,
-		autoplay: true,
-		autoplaySpeed: 3000,
+/* 		autoplay: true,
+		autoplaySpeed: 3000, */
 		prevArrow:'<button type="button" class="slick-prev "><img src ="icons/left.svg"> </button>',
 		nextArrow:'<button type="button" class="slick-next"><img src ="icons/right.svg"> </button>',
 		responsive: [
 			{
 			  breakpoint: 992,
 			  settings: {
-				arrows: false
-			  }
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				infinite: true,
+				}
 			},
 			{
 			  breakpoint: 767,
 			  settings: {
-				arrows: false,
+				slidesToShow: 1,
+				slidesToScroll: 1	
+			  }
+			},
+			{
+			  breakpoint: 576,
+			  settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1
 			  }
 			}
 		  ]
