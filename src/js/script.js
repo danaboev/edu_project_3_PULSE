7a39e717ab06917374da54d3d18243a988104666
jahon <jahongir.danaboyev@mail.ru>
2023-04-16 14:23:10 +0300

    mailer

--- src/js/script.js
+++ src/js/script.js
@@ -99,5 +99,23 @@ validateForms('#order form');
 //phone mask
 
 $('input[name=phone]').mask("+7 (999) 999-99-99");
+$('form').submit(function(e){
+	e.preventDefault();
 
+	if(!$(this).valid()){
+		return;
+	}
+	$.ajax({
+		type:"POST",
+		url: "mailer/smart.php",
+		data: $(this).serialize()
+	}).done(function(){
+		$(this).find("input").val("");
+		$('#consultation, #oder').fadeOut();
+		$('.overlay, #thaks').fadeIn('slow');
+
+		$('form').trigger('reset');
+	});
+	return false;
+});
 });
